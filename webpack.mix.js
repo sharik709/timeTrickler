7553d748ca1cc7df52d9e768bd81d0a00a1bbb85
Sharik shaikh <shaikhsharik709@gmail.com>
2019-03-10 17:21:11 +0530

    added tailwindcss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,5 +12,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix
+    .js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        tailwindcss('./tailwind.js')
+    ]);
