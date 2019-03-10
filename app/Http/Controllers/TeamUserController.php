33797b820c154db7c8724a00bc8c07e2baa91cf5
Sharik shaikh <shaikhsharik709@gmail.com>
2019-03-10 15:53:42 +0530

    most part for adding a user and removing a user from team functionality added

--- app/Http/Controllers/TeamUserController.php
+++ app/Http/Controllers/TeamUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TeamUser\TeamUserDestroyRequest;
 use App\Team;
 use App\User;
 use Illuminate\Http\Request;
@@ -15,12 +16,26 @@ class TeamUserController extends Controller
      */
     public function store(Team $team, Request $request)
     {
-        if( $team->hasReachedUserLimit() ) {
+        if( $team->hasReachedUsersLimit() ) {
             return back()
                         ->with('You can only add upto '.$team->users_limit .' Users');
         }
         $user = User::findByUuidOrFail($request->user);
         $team->addUser($user);
+        return redirect('/employer/team/'.$team->uuid);
+    }
+
+    /**
+     * @param Request $request
+     * @param Team $team
+     * @param User $user
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
+    public function destroy(TeamUserDestroyRequest $request, Team $team, User $user)
+    {
+        $this->authorize('destroy', $team);
+        $team->removeUser($user);
         return redirect('/employer/team/'.$team->uuid);
     }
 }
--- tests/Feature/Controllers/TeamUserControllerTest.php
+++ tests/Feature/Controllers/TeamUserControllerTest.php
@@ -30,13 +30,68 @@ class TeamUserControllerTest extends TestCase
     {
         $employer = $this->employer();
         $team = factory(Team::class)->create(['user_id' => $employer->id]);
+        $team->addUser($this->employee());
+        $team->addUser($this->employee());
+        $team->addUser($this->employee());
+
         $user = $this->employee();
         $this
             ->actingAs($employer)
             ->post('/employer/team/'.$team->uuid.'/user', [
                 'user' => $user->uuid
             ])
+            ->assertRedirect('/');
+        $this->assertFalse($team->users->contains($user));
+    }
+
+    /** @test */
+    public function check_if_user_can_be_removed_from_a_team()
+    {
+        $employer = $this->employer();
+        $employer->givePermissionTo('team:user:delete');
+        $team = factory(Team::class)->create(['user_id' => $employer->id]);
+        $user = $this->employee();
+        $team->addUser($user);
+        $team->addUser($this->employee());
+        $team->addUser($this->employee());
+
+        $this
+            ->actingAs($employer)
+            ->delete('/employer/team/'.$team->uuid.'/user/'.$user->uuid)
             ->assertRedirect('/employer/team/'.$team->uuid);
         $this->assertFalse($team->users->contains($user));
+    }
+
+    public function test_if_only_who_has_permission_can_remove_user_from_team()
+    {
+        $employer = $this->employer();
+        $team = factory(Team::class)->create(['user_id' => $employer->id]);
+        $user = $this->employee();
+        $team->addUser($user);
+        $team->addUser($this->employee());
+        $team->addUser($this->employee());
+
+        $this
+            ->actingAs($employer)
+            ->delete('/employer/team/'.$team->uuid.'/user/'.$user->uuid)
+            ->assertForbidden();
+        $this->assertTrue($team->users->contains($user));
+    }
+
+    public function test_if_only_who_has_created_team_can_remove_user_from_team()
+    {
+        $employer = $this->employer();
+        $employer->givePermissionTo('team:user:delete');
+        $team = factory(Team::class)->create();
+        $user = $this->employee();
+        $team->addUser($user);
+        $team->addUser($this->employee());
+        $team->addUser($this->employee());
+
+        $response = $this
+            ->actingAs($employer)
+            ->delete('/employer/team/'.$team->uuid.'/user/'.$user->uuid)
+            ->assertForbidden();
+        $this->assertTrue($team->users->contains($user));
     }
 }
